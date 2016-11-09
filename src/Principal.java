1719daacaaf5725ffb76cd9e2120dd8a3dcae017
UserPL135Pc06 <UserPL135Pc06@SLFRPL135PC06.ensino.atec.local>
2016-11-09 09:37:41 +0000

    Arrays

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,4 @@
-
+import dia091116.TreinoArrays;
 
 public class Principal {
 
@@ -8,17 +8,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		String mensagem = "Olá turma, de mensagem";
-		
-		
-		int agua = 40;
-		int luz = 20;
-		int gas = 20;
-		int fibra = 60;
-		int despesas = agua + luz + gas + fibra;
-		// TODO Mostrar no ecra o total de despesas
-		System.out.println(despesas);
+		new TreinoArrays();
 	}
 
 }
---
+++ src/dia091116/TreinoArrays.java
@@ -0,0 +1,35 @@
+package dia091116;
+
+public class TreinoArrays {
+
+	public TreinoArrays() {
+		
+		// Numero de golos - Porto-Benfica
+		// Tipo de variavel; [] Array; Nome de variavel; Inicializacao
+		// Tamanho de variavel
+		int[] golos = new int[2];
+		
+		// Atribuir variaveis ao Array
+		golos[0] = 1;
+		golos[1] = 1;
+		//System.out.println(golos[0]);
+		
+		// Instanciacao com inicializacao de variaveis
+		int[] portobenfica = {1, 1};
+		int[] sportingarouca = {3, 0};
+		int[] maritimobraga = {1, 0};
+		int[][] jogosgrandes = new int[2][3];
+		
+		jogosgrandes[0][0] = portobenfica[0];
+		jogosgrandes[1][0] = portobenfica[1];
+		
+		jogosgrandes[0][1] = sportingarouca[0];
+		jogosgrandes[1][1] = sportingarouca[1];
+		
+		jogosgrandes[0][2] = maritimobraga[0];
+		jogosgrandes[1][2] = maritimobraga[1];
+		
+		System.out.println(jogosgrandes[0][0]);
+	}
+
+}
