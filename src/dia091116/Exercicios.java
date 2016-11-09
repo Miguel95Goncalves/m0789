e4eb0b3955e3a5f8f7e2b2605a7ff901ff31e121
UserPL135Pc06 <UserPL135Pc06@SLFRPL135PC06.ensino.atec.local>
2016-11-09 11:10:23 +0000

    Desafios

--- src/dia091116/Exercicios.java
+++ src/dia091116/Exercicios.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicios {
 
 	public Exercicios() {
-		exercicio2();
+		exercicio3();
 		
 		
 	}
@@ -66,7 +66,8 @@ public class Exercicios {
 	public void exercicio3() {
 		//Exercício 3 Nível 3
 		// Utilize uma matriz 3 por 3
-		// Peça ao utilizador para preencher a matriz, no final apresente a soma de todos os valores e apresente graficamente a matriz
+		// Peça ao utilizador para preencher a matriz
+		// No final apresente a soma de todos os valores e apresente graficamente a matriz
 		
 		int[][] matriz = new int[3][3];
 		int soma=0, num;
@@ -81,12 +82,16 @@ public class Exercicios {
 		}
 		
 		System.out.println("\nSoma = " + soma + "\n");
-		for (int i=0;i<=2;i++) {
-			for (int o=0;o<=2;o++) {
-				System.out.println(matriz[i][o]);
-			}
-			System.out.println("\n");
-		}
+		//for (int i=0;i<=2;i++) {
+			//for (int o=0;o<=2;o++) {
+				System.out.println(matriz[0][0] + " " + matriz[0][1] + " " + matriz[0][2]);
+				
+				System.out.println(matriz[1][0] + " " + matriz[1][1]  + " " + matriz[1][2]);
+				
+				System.out.println(matriz[2][0] + " " +  matriz[2][1] + " " + matriz[2][2]);
+			//}
+			//System.out.println("\n");
+		//}
 		
 	}
 
